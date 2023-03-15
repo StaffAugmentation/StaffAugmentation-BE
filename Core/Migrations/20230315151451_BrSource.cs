e5943e3273d0d0ce4116978cb920ba3d629f9903
Alae Benhamza <alaebenhamza3@gmail.com>
2023-03-15 17:29:57 +0100

    BrSource

---
+++ Core/Migrations/20230315151451_BrSource.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class BrSource : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BrSource",
+                columns: table => new
+                {
+                    IdSource = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    SourceName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BrSource", x => x.IdSource);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BrSource");
+        }
+    }
+}
