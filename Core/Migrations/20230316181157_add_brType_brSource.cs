3d772fd442a29d4405c6253c3f303b981434df5e
Zayd EL IMRANI <elimrani.z@gmail.com>
2023-03-16 19:18:52 +0100

    Creating Migrations for BrSource and BrType

--- Core/Migrations/20230316181157_add_brType_brSource.cs
+++ Core/Migrations/20230316181157_add_brType_brSource.cs
@@ -5,23 +5,35 @@
 namespace Core.Migrations
 {
     /// <inheritdoc />
-    public partial class add_profile : Migration
+    public partial class add_brType_brSource : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Profile",
+                name: "BrSource",
+                columns: table => new
+                {
+                    IdSource = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    SourceName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BrSource", x => x.IdSource);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BrType",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     ValueId = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    IsActive = table.Column<bool>(type: "bit", nullable: true)
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Profile", x => x.Id);
+                    table.PrimaryKey("PK_BrType", x => x.Id);
                 });
         }
 
@@ -29,7 +41,10 @@ namespace Core.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Profile");
+                name: "BrSource");
+
+            migrationBuilder.DropTable(
+                name: "BrType");
         }
     }
 }
