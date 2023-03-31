17fbf3885811a8bd3ea1753b71ea7a2296a1cfd0
Zayd EL IMRANI <elimrani.z@gmail.com>
2023-03-31 22:32:46 +0000

    Adding test on status on TestApproverController

--- UnitTests/Controllers/TestApproverController.cs
+++ UnitTests/Controllers/TestApproverController.cs
@@ -5,24 +5,35 @@ using API.Controllers;
 using Business.IServices;
 using Core.ViewModel;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using FluentAssertions;
 
 namespace UnitTests.Controllers;
 
 public class TestApproverController
 {
     private readonly ApproverController _controller;
+    private ApproverViewModel testApprover = new ApproverViewModel { Id = 4, FirstName = "First name 4", LastName = "Last name 4" };
 
     public TestApproverController()
     {
         var loggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); });
         var logger = loggerFactory.CreateLogger<ApproverController>();
+
+        List<ApproverViewModel> approvers = new List<ApproverViewModel>
+        {
+            new ApproverViewModel { Id = 1, FirstName = "First name 1", LastName = "Last name 1" },
+            new ApproverViewModel { Id = 2, FirstName = "First name 2", LastName = "Last name 2" },
+            new ApproverViewModel { Id = 3, FirstName = "First name 3", LastName = "Last name 3" }
+        };
+
         var mockApproverService = new Mock<IApproverService>();
-        mockApproverService.Setup(svc => svc.GetApprover().Result).Returns(new List<ApproverViewModel>
-            {
-                new ApproverViewModel { Id = 1, FirstName = "First name 1", LastName = "Last name 1" },
-                new ApproverViewModel { Id = 2, FirstName = "First name 2", LastName = "Last name 2" },
-                new ApproverViewModel { Id = 3, FirstName = "First name 3", LastName = "Last name 3" }
-            });
+        mockApproverService.Setup(svc => svc.GetApprover().Result).Returns(approvers);
+        mockApproverService.Setup(svc => svc.GetApprover(1).Result).Returns(approvers.FirstOrDefault());
+        mockApproverService.Setup(svc => svc.CreateApprover(testApprover).Result).Returns(approvers.FirstOrDefault());
+        mockApproverService.Setup(svc => svc.UpdateApprover(testApprover).Result).Returns(approvers.FirstOrDefault());
+        mockApproverService.Setup(svc => svc.DeleteApprover(1).Result).Returns(approvers);
+
         _controller = new ApproverController(logger, mockApproverService.Object);
     }
 
@@ -30,52 +41,64 @@ public class TestApproverController
     public async Task Get_OnReturnsData()
     {
         // Act
-        var result = await _controller.GetAll();
+        var result = (await _controller.GetAll()).Result as OkObjectResult;
 
-        // Assert
-        Assert.IsType<ActionResult<List<ApproverViewModel>?>?>(result);
+        // check data
+        Assert.IsType<List<ApproverViewModel>?>(result?.Value);
 
-        //result.Should().BeOfType<ActionResult<List<ApproverViewModel>?>?>()
-        //    .Which.Result.Should().Be((int)HttpStatusCode.OK);
+        // check status
+        result.StatusCode.Should().Be((int)HttpStatusCode.OK);
     }
 
     [Fact]
     public async Task GetApprover_OnReturnsData()
     {
         // Act
-        var result = await _controller.GetApprover(100);
+        var result = (await _controller.GetApprover(1)).Result as OkObjectResult;
 
-        // Assert
-        Assert.IsType<ActionResult<ApproverViewModel?>?>(result);
+        // check data
+        Assert.IsType<ApproverViewModel?>(result?.Value);
+
+        // check status
+        result.StatusCode.Should().Be((int)HttpStatusCode.OK);
     }
 
     [Fact]
     public async Task Create_OnReturnsData()
     {
         // Act
-        var result = await _controller.CreateApprover(new ApproverViewModel { Id = 4, FirstName = "First name 4", LastName = "Last name 4" });
+        var result = (await _controller.CreateApprover(testApprover)).Result as ObjectResult;
 
-        // Assert
-        Assert.IsType<ActionResult<ApproverViewModel?>>(result);
+        // check data
+        Assert.IsType<ApproverViewModel?>(result?.Value);
+
+        // check status
+        result.StatusCode.Should().Be((int)HttpStatusCode.Accepted);
     }
 
     [Fact]
     public async Task Update_OnReturnsData()
     {
         // Act
-        var result = await _controller.UpdateApprover(new ApproverViewModel { Id = 4, FirstName = "First name updated 4" });
+        var result = (await _controller.UpdateApprover(testApprover)).Result as ObjectResult;
 
-        // Assert
-        Assert.IsType<ActionResult<ApproverViewModel?>>(result);
+        // check data
+        Assert.IsType<ApproverViewModel?>(result?.Value);
+
+        // check status
+        result.StatusCode.Should().Be((int)HttpStatusCode.Accepted);
     }
 
     [Fact]
     public async Task Delete_OnReturnsData()
     {
         // Act
-        var result = await _controller.DeleteApprover(4);
+        var result = (await _controller.DeleteApprover(1)).Result as ObjectResult;
 
-        // Assert
-        Assert.IsType<ActionResult<List<ApproverViewModel>>>(result);
+        // check data
+        Assert.IsType<List<ApproverViewModel>?>(result?.Value);
+
+        // check status
+        result.StatusCode.Should().Be((int)HttpStatusCode.Accepted);
     }
 }
