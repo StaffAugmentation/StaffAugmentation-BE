e89118dd044e8658ad025483786e2f99e8eab6a7
hajjabri <zayd.imr@hotmail.com>
2023-05-17 16:42:09 +0100

    AdvancedSearchVM

---
+++ Core/ViewModel/AdvancedSearchViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.ViewModel;
+
+public class AdvancedSearchViewModel
+{
+    public string RequestNumber { get; set; }
+    public DateTime? AdvancedSearchDate { get; set; }
+    public List<string> AdvancedSearchList { get; set; }
+    public List<string> AdvancedSearchListFWC { get; set; }
+    public DateTime? PaymentSchedule { get; set; }
+}
